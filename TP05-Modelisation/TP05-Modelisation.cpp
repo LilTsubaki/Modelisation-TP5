3d099d57a66169c02a129d02e900267a51f34ce5
LilTsubaki <omenforce.clem@neuf.fr>
2015-10-14 10:07:37 +0200

    tp5 exo 1 ok

--- TP05-Modelisation/TP05-Modelisation.cpp
+++ TP05-Modelisation/TP05-Modelisation.cpp
@@ -1,215 +1,141 @@
+/*
+ * Copyright (c) 1993-1997, Silicon Graphics, Inc.
+ * ALL RIGHTS RESERVED
+ * Permission to use, copy, modify, and distribute this software for
+ * any purpose and without fee is hereby granted, provided that the above
+ * copyright notice appear in all copies and that both the copyright notice
+ * and this permission notice appear in supporting documentation, and that
+ * the name of Silicon Graphics, Inc. not be used in advertising
+ * or publicity pertaining to distribution of the software without specific,
+ * written prior permission.
+ *
+ * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
+ * AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
+ * GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY
+ * KIND, OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION,
+ * LOSS OF PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF
+ * THIRD PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
+ * POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * US Government Users Restricted Rights
+ * Use, duplication, or disclosure by the Government is subject to
+ * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
+ * (c)(1)(ii) of the Rights in Technical Data and Computer Software
+ * clause at DFARS 252.227-7013 and/or in similar or successor
+ * clauses in the FAR or the DOD or NASA FAR Supplement.
+ * Unpublished-- rights reserved under the copyright laws of the
+ * United States.  Contractor/manufacturer is Silicon Graphics,
+ * Inc., 2011 N.  Shoreline Blvd., Mountain View, CA 94039-7311.
+ *
+ * OpenGL(R) is a registered trademark of Silicon Graphics, Inc.
+ */
+
+/*
+ * hello.c
+ * This is a simple, introductory OpenGL program.
+ */
 #include "stdafx.h"
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <math.h>
-#include "glut.h" 
-#include "Maillage.h"
-
-#define WIDTH  480
-#define HEIGHT 480
- 
-#define RED   0
-#define GREEN 0
-#define BLUE  0
-#define ALPHA 1
-#define PI 3.14159
- 
-#define KEY_ESC 27
- 
-void init_scene();
-void render_scene();
-GLvoid initGL();
-GLvoid window_display();
-GLvoid window_reshape(GLsizei width, GLsizei height); 
-GLvoid window_key(unsigned char key, int x, int y); 
-GLvoid window_idle(); 
- 
-// Paramètres de la source de lumière 0 (directionnelle)
-GLfloat Light0Position[]= { 4.0f, 4.0f, 2.0f, 0.0f };
-GLfloat Light0Ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-GLfloat Light0Diffuse[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-GLfloat Light0Specular[]= { 0.3f, 0.3f, 0.3f, 1.0f };
- 
-// Paramètres de la source de lumière 1 (ponctuelle)
-GLfloat Light1Position[]= { 4.0f, 8.0f, 2.0f, 1.0f };
-GLfloat Light1Ambient[] = { 0.1f, 0.1f, 0.1f, 1.0f };
-GLfloat Light1Diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-GLfloat Light1Specular[]= { 0.3f, 0.3f, 0.3f, 1.0f };
-
-static double zRotate = 0.5;
-static double facteur = 0.5;
-static Maillage maille;
-
-int main(int argc, char **argv) 
-{  
-  // initialisation  des paramètres de GLUT en fonction
-  // des arguments sur la ligne de commande
-
-	
-
-  glutInit(&argc, argv);
-     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
- 
-	 maille = Maillage::lectureOff("C:/Users/etu/Desktop/bunny.off", 10);
-	 maille.calculNormalTriangle();
-    //bunny.Ecriture("bunny.obj");
+#include <cstdlib>
+#include "glut.h"
+#include <math.h> 
+#include <iostream>
+#include "Vector3d.h"
+#define M_PI 3.14159265358979323846
 
 
-  // définition et création de la fenêtre graphique
-  glutInitWindowSize(WIDTH, HEIGHT);
-  glutInitWindowPosition(0, 0);
-  glutCreateWindow("Primitives graphiques");
- 
-  // initialisation de OpenGL et de la scène
-  initGL();  
-  init_scene();
- 
-  // choix des procédures de callback pour 
-  // le tracé graphique
-  glutDisplayFunc(&window_display);
-  // le redimensionnement de la fenêtre
-  glutReshapeFunc(&window_reshape);
-  // la gestion des événements clavier
-  glutKeyboardFunc(&window_key);
-  glutIdleFunc(&window_idle);
-  // la boucle prinicipale de gestion des événements utilisateur
-  glutMainLoop();  
- 
-  return 1;
-}
- 
-GLvoid afficheMaille()
+
+GLvoid hermite(Vector3D p0, Vector3D p1, Vector3D v0, Vector3D v1)
 {
-	Vector3D a;
-	Vector3D b;
-	Vector3D c;
-	double facteur = 100.0;
-	int cptNormale = 0;
+	double u = 0;
+	double f1;
+	double f2;
+	double f3;
+	double f4;
+	double x;
+	double y;
+	double z;
 
-	std::vector<Vector3D> geometrie = maille.getGeom();
-	std::vector<Vector3D> normales = maille.getNormales();
-    std::vector<int> topologie = maille.getTopo();
-	Vector3D centre = maille.getCentreGravite();
-	double scale = maille.getScale();
-
-	//std::cout << maille.getTopo().size() << std::endl;
-	for(int i =0; i < topologie.size(); i+=3)
+	glBegin(GL_LINE_STRIP);
+		glColor3f (1.0, 0.0, 1.0);
+	for(int i = 0; i <= 10; i++)
 	{
-		a = (geometrie.at(topologie.at(i))-centre)/scale;
-		b = (geometrie.at(topologie.at(i+1))-centre)/scale;
-		c = (geometrie.at(topologie.at(i+2))-centre)/scale;
-		//std::cout << "a : " << a.x() << " | " <<  a.y()<< " | " <<  a.z() << std::endl;
-		//std::cout << "b : " << b.x() << " | " <<  b.y()<< " | " <<  b.z() << std::endl;
-		//std::cout << "c : " << c.x() << " | " <<  c.y()<< " | " <<  c.z() << std::endl;
+		f1 = 2*u*u*u-3*u*u+1;
+		f2 = -2*u*u*u+3*u*u;
+		f3 = u*u*u-2*u*u+u;
+		f4 = u*u*u-u*u;
 
-		glNormal3d(normales.at(cptNormale).x(), normales.at(cptNormale).y(), normales.at(cptNormale).z());
-		//std::cout << normales.at(cptNormale).x() << " | " <<  normales.at(cptNormale).y()<< " | " <<  normales.at(cptNormale).z() << std::endl;
-		glBegin(GL_TRIANGLES);
-			glVertex3f (a.x(), a.y(), a.z());
-			glVertex3f (b.x(), b.y(), b.z());
-			glVertex3f (c.x(), c.y(), c.z());
-	   glEnd();
+		x = f1*p0.x()+f2*p1.x()+f3*v0.x()+f4*v1.x();
+		y = f1*p0.y()+f2*p1.y()+f3*v0.y()+f4*v1.y();
+		z = f1*p0.z()+f2*p1.z()+f3*v0.z()+f4*v1.z();
 
-	   cptNormale++;
+		
+		glVertex3f (x, y, z);
+
+		u+=0.1;
 	}
-
-	//std::cout << "endload" << std::endl;
+	glEnd();
 }
 
 
-// initialisation du fond de la fenêtre graphique : noir opaque
- 
-GLvoid initGL() 
+void display(void)
 {
-	glClearColor(0.6f, 0.6f, 0.7f, 1.0f);               // Couleur servant à effacer la fenêtre (celle du brouillard)
-    glShadeModel(GL_SMOOTH);                            // Modèle d'ombrage : lissage de Gouraud
-    //glEnable(GL_CULL_FACE);                             // Ne traitre pas les faces cachées
-    glEnable(GL_DEPTH_TEST);                            // Active le Z-Buffer
-    glDepthFunc(GL_LEQUAL);                             // Fonctionnement du Z-Buffer
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Active la correction de perspective (pour ombrage, texture, ...)
- 
-    glLightfv(GL_LIGHT0, GL_AMBIENT, Light0Ambient);    // Couleur ambiante de la source de lumière 0 (directionnelle)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, Light0Diffuse);    // Couleur diffuse de la source de lumière 0
-    glLightfv(GL_LIGHT0, GL_SPECULAR,Light0Specular);   // Couleur spéculaire de la source de lumière 0
-    glLightfv(GL_LIGHT0, GL_POSITION,Light0Position);   // Direction de la source de lumière 0
-    glEnable(GL_LIGHT0);                                // Activation de la source de lumière 0
- 
-    glLightfv(GL_LIGHT1, GL_AMBIENT, Light1Ambient);    // Couleur ambiante de la source de lumière 1
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, Light1Diffuse);    // Couleur diffuse de la source de lumière 1
-    glLightfv(GL_LIGHT1, GL_SPECULAR,Light1Specular);   // Couleur spéculaire de la source de lumière 1
-    glLightfv(GL_LIGHT1, GL_POSITION,Light1Position);   // Position de la source de lumière 1
-    glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.02);   // Atténuation de la source de lumière 1
-    //glEnable(GL_LIGHT1);                              // Activation de la source de lumière 1
- 
-    glEnable(GL_LIGHTING);                              // Activation de l'éclairage général
+/* clear all pixels  */
+   glClear (GL_COLOR_BUFFER_BIT);
 
-        glShadeModel(GL_SMOOTH);
-    glClearColor(RED, GREEN, BLUE, ALPHA);
-        glEnable(GL_DEPTH_TEST);
-}
- 
-void init_scene()
-{
-}
- 
-
-
-
-// fonction de call-back pour l´affichage dans la fenêtre
-GLvoid window_display()
-{
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 
-  render_scene();
-  glRotated(1,0,1,0);
-  	 afficheMaille();
-  // trace la scène grapnique qui vient juste d'être définie
-  glFlush();
-  glutSwapBuffers();
+/* draw white polygon (rectangle) with corners at
+ * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0) 
+ */
   
-}
- 
-// fonction de call-back pour le redimensionnement de la fenêtre
- 
-GLvoid window_reshape(GLsizei width, GLsizei height)
-{  
-  glViewport(0, 0, width, height);
- 
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-15.0, 15.0, -15.0, 15.0, -15.0, 15.0);
- 
-  // toutes les transformations suivantes s´appliquent au modèle de vue 
-  glMatrixMode(GL_MODELVIEW);
-}
- 
-// fonction de call-back pour la gestion des événements clavier
- 
-GLvoid window_key(unsigned char key, int x, int y) 
-{  
-  switch (key) {    
-  case KEY_ESC:  
-    exit(1);                    
-    break;
-     
 
-  default:
-    printf ("La touche %d n´est pas active.\n", key);
-    break;
-  }     
+   Vector3D p0(0,0,0);
+	Vector3D p1(1,0,0);
+	Vector3D v0(1,1,0);
+	Vector3D v1(1,-1,0);
+
+  hermite(p0,p1,v0,v1);
+
+
+/* Swap the buffers to show the one
+ * on which we writed
+ */
+   glutSwapBuffers();
 }
- 
-GLvoid window_idle() 
-{  
-	//zRotate+=facteur;
-	glutPostRedisplay();
-}
- 
- 
-void render_scene()
+
+
+
+
+void init (void)
 {
-//c'est ici qu'on dessine
-    //glLoadIdentity();
- 
- 
- }
+/* select clearing color     */
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+
+/* initialize viewing values  */
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+/*
+ * Declare initial window size, position, and display mode
+ * (double buffer and RGB).  Open window with "hello"
+ * in its title bar.  Call initialization routines.
+ * Register callback function to display graphics.
+ * Enter main loop and process events.
+ */
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+   glutInitWindowSize (500, 500);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow ("hello");
+   init ();
+   glutDisplayFunc(display);
+   glutMainLoop();
+   return 0;   /* ANSI C requires main to return int. */
+}
